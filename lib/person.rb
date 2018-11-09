33f2d5f24819c67760e0b3016477f8fa5ca1da98
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-09 01:19:28 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,7 @@ class Person
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
 
   # attributes.each {|key, value| self.send(("#{key}="), value)}
